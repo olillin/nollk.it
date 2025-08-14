56552e6ba16de693f082c14c94a77ae1b4f6e64a
Oli <oli@olillin.com>
2025-08-15 00:40:02 +0200

    Add section to edit timeline categories

--- components/ManagementDisplays/TimelineManagementDisplay.tsx
+++ components/ManagementDisplays/TimelineManagementDisplay.tsx
@@ -11,12 +11,16 @@ interface TimelineManagementDisplayProps {
 
 export default function TextManagementDisplay(props: TimelineManagementDisplayProps) {
 
-  const [selectedYear, setSelectedYear] = useState(props.committees.sort((a, b) => b.year - a.year)[0].year)
+  const [selectedYear, setSelectedYear] = useState(props.committees.sort((a, b) => b.year - a.year)[0]?.year ?? new Date().getFullYear())
 
   const [shownEvents, setShownEvents] = useState(props.timelineEvents.filter(event => event.year === selectedYear.toString()).sort((a, b) => a.date > b.date ? 1 : -1))
 
-  const getCategoryColor = (categoryId: string) => {
-    return props.categories.find(category => category.title === categoryId)?.color ?? props.categories[0].color
+  const [shownCategories, setShownCategories] = useState<Category[]>(props.categories)
+
+  const [newCategory, setNewCategory] = useState<Category>({ title: "", color: "", })
+
+  const getCategoryColor = (categoryId: string): string | undefined => {
+    return shownCategories.find(category => category.title === categoryId)?.color ?? shownCategories[0]?.color
   }
 
   return <>
@@ -32,7 +36,10 @@ export default function TextManagementDisplay(props: TimelineManagementDisplayPr
       }
     </select>
 
-    <div className="flex flex-col gap-8 pb-8">
+    <section className="mb-8">
+      <h2 className="text-2xl mb-4">Tidslinje</h2>
+
+      <div className="flex flex-col gap-8 pb-8">
       {
         shownEvents.map((event, index) => {
           return (
@@ -85,15 +92,16 @@ export default function TextManagementDisplay(props: TimelineManagementDisplayPr
           )
         })
       }
+      </div>
 
       <div className="flex gap-4">
-        <select id="category-select" className="p-1 rounded-md" style={{ backgroundColor: props.categories[0].color }} onChange={
+        <select id="category-select" className="p-1 rounded-md" style={{ backgroundColor: shownCategories[0]?.color ?? "" }} onChange={
           e => {
-            e.target.style.backgroundColor = getCategoryColor(e.target.value)
+            e.target.style.backgroundColor = getCategoryColor(e.target.value) ?? ""
           }
         }>
           {
-            props.categories.map(category => {
+            shownCategories.map(category => {
               return <option style={{ backgroundColor: category.color }} key={category.title} value={category.title}>
                 {category.title}
               </option>
@@ -116,8 +124,7 @@ export default function TextManagementDisplay(props: TimelineManagementDisplayPr
         </Button>
       </div>
 
-      <div>
-
+      <div className="mt-8">
         <Button color="bg-green-500" action={() => {
           fetch('/api/admin/timeline/update', {
             method: 'POST',
@@ -140,10 +147,91 @@ export default function TextManagementDisplay(props: TimelineManagementDisplayPr
         }}>
           Spara
         </Button>
+      </div>
+    </section>
 
+    <section className="mb-8">
+      <h2 className="text-2xl mb-6">Kategorier</h2>
+      <div className="flex flex-col gap-4 pb-4">
+        {
+          shownCategories.map((category, index) => {
+            return (
+              <div key={category.title} className="bg-black/50 p-5">
+
+                <div className="flex items-start justify-between">
+                  <div className="p-1 px-2" style={{ backgroundColor: getCategoryColor(category.title) }}>{category.title}</div>
+                  <Button color="bg-red-500" action={() => {
+                    const usedEvent = props.timelineEvents.find(event => event.categoryId === category.title)
+                    if (usedEvent) {
+                      alert(`Kan inte ta bort kategori, används år ${usedEvent.year} med text "${usedEvent.text}"`)
+                      return
+                    }
+
+                    const newShownCategories = [...shownCategories]
+                    newShownCategories.splice(index, 1)
+                    setShownCategories(newShownCategories)
+                  }}>
+                    Ta bort
+                  </Button>
+                </div>
+
+                <TextInput placeholder="Färg" setValue={
+                  inputValue => {
+                    const newShownCategories = [...shownCategories]
+                    newShownCategories[index].color = inputValue
+                    setShownCategories(newShownCategories)
+                  }
+                }>
+                  {category.color}
+                </TextInput>
+              </div>
+            )
+          })
+        }
       </div>
 
-    </div >
+      <span className="flex flex-row items-center gap-4">
+          <TextInput
+            placeholder="Titel på kategori"
+            setValue={inputValue => {
+              const changedCategory = { ...newCategory, title: inputValue }
+              setNewCategory(changedCategory)
+            }}
+          >
+            {newCategory.title}
+          </TextInput>
 
+          <Button action={async () => {
+            setShownCategories([...shownCategories, newCategory])
+            setNewCategory({...newCategory, title: ""})
+          }}>
+            Lägg till
+          </Button>
+      </span>
+
+      <div className="mt-8">
+        <Button color="bg-green-500" action={() => {
+          fetch('/api/admin/category/update', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+              {
+                categories: shownCategories
+              }
+            )
+          }).then(res => {
+            if (res.status === 200) {
+              alert("Sparat!")
+            } else {
+              alert("Något gick fel")
+            }
+          })
+        }}>
+          Spara
+        </Button>
+      </div>
+    </section>
   </>
 }
