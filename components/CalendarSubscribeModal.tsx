95c9f961517a529a5c37a5f7a45d84cfcaf3e5d4
Oli <oli@olillin.com>
2025-08-15 23:09:50 +0200

    Add copy calendar link button

--- components/CalendarSubscribeModal.tsx
+++ components/CalendarSubscribeModal.tsx
@@ -1,3 +1,4 @@
+import Button from './Button'
 import Modal from './Modal'
 
 type CalendarSubscribeModalProps = {
@@ -11,18 +12,57 @@ const CalendarSubscribeModal = (props: CalendarSubscribeModalProps) => {
         <Modal onClose={props.onClose} background={props.background}>
             <h2 className="text-2xl mb-4 pr-8">Lägg till i kalender</h2>
 
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
                 <CalendarLink
-                    href={`https://www.google.com/calendar/render?cid=${props.url}`}
+                    href={`https://calendar.google.com/calendar/r?cid=${encodeURIComponent(
+                        props.url
+                    )}`}
                 >
                     Google Kalender
                 </CalendarLink>
                 <CalendarLink href={props.url}>Apple Kalender</CalendarLink>
                 <CalendarLink
-                    href={`https://outlook.office.com/owa/?path=/calendar/action/compose&rru=addsubscription&url=${props.url}`}
+                    href={`https://outlook.office.com/owa/?path=/calendar/action/compose&rru=addsubscription&url=${encodeURIComponent(
+                        props.url
+                    )}`}
                 >
                     Outlook
                 </CalendarLink>
+            </div>
+
+            <div className="mt-8">
+                <p>Eller kopiera länken nedan:</p>
+                <input
+                    type="text"
+                    id="calendarLink"
+                    disabled
+                    aria-disabled="true"
+                    value={props.url}
+                    className="w-full px-1 rounded-sm bg-white text-black mt-2 mb-4"
+                />
+
+                <div className="w-full flex">
+                    <span className="ml-auto">
+                        <Button
+                            action={() => {
+                                console.log('Copying calendar link')
+
+                                const inputElement = document.getElementById(
+                                    'calendarLink'
+                                ) as HTMLInputElement
+
+                                inputElement.select()
+                                inputElement.setSelectionRange(0, 99999)
+
+                                navigator.clipboard.writeText(props.url)
+
+                                alert('Kopierat!')
+                            }}
+                        >
+                            Kopiera
+                        </Button>
+                    </span>
+                </div>
             </div>
         </Modal>
     )
